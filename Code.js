30f8c007f4f6953d8ea359d4c1c8eda50717f3ea
Nick Frayter <64379365+fraytern@users.noreply.github.com>
2022-11-09 16:15:44 -0500

    Copied over website files to repo

---
+++ Code.js
@@ -0,0 +1,9 @@
+document.getElementById("date-btn").addEventListener("click",colorfix);
+function colorfix(){
+    document.getElementById("date-btn").innerText = Date();
+}
+/*
+function colorchange(){
+    formhead.style.color = 'blue';
+}
+*/
